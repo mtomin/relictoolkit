1f5fdb86a9b4e0184755eaa05d720e48f070634a
menta <mtomin.chem@gmail.com>
2018-10-16 07:02:59 +0200

    Expanded unit tests
    Signed-off-by: menta <mtomin.chem@gmail.com>

--- tests/test_calculation.py
+++ tests/test_calculation.py
@@ -9,6 +9,7 @@ import MDAnalysis
 import relictoolkit.calculation as c
 import os
 import mock
+from configparser import ConfigParser
 standard_library.install_aliases()
 
 
@@ -18,6 +19,7 @@ def test_add_lj_parameters():
     system = MDAnalysis.Universe(topology, trajectory, format='mdcrd')
     c.add_lj_parameters(system)
     assert system.atoms[0].lj_energy == 3.66
+    assert system.atoms[0].vdw_radius == 0.069
 
 
 def test_timeit():
@@ -41,8 +43,9 @@ def do_nothing(*args):
     pass
 
 
+@mock.patch('relictoolkit.calculation.add_lj_parameters', side_effect=do_nothing)
 @mock.patch('relictoolkit.utils.process_frame', side_effect=do_nothing)
-def test_process_trajectory(self):
+def test_process_trajectory(*args):
 
     topology = os.path.dirname(__file__) + '/data/testtop.prmtop'
     mask1 = 'resid 3'
@@ -73,3 +76,36 @@ def test_process_trajectory(self):
     os.remove('output.dat_0')
     os.remove(os.path.dirname(__file__) + '/data/testtraj.mdcrd')
 
+
+@mock.patch('relictoolkit.utils.write_logfile_header', side_effect=do_nothing())
+@mock.patch('multiprocessing.Pool', side_effect=do_nothing())
+def test_perform_analysis(*args):
+    # Create a test config file
+    test_config = ConfigParser()
+    test_config['files'] = {
+        'topology': os.path.dirname(__file__) + '/data/testtop.prmtop',
+        'trajectories': os.path.dirname(__file__) + '/data/testtraj.xcrd',
+        'filetype': 'mdcrd'
+    }
+    test_config['parameters'] = {
+        'mask1': 'resid 3',
+        'mask2': 'resid 4',
+        'stride': 1,
+        'ncores': 1,
+        'dt': 2
+    }
+
+    with open('testrun_config.ini', 'w+') as f:
+        test_config.write(f)
+
+    c.perform_analysis('testrun_config.ini')
+    os.remove('testrun_config.ini')
+
+    assert os.path.isfile('output.dat')
+    assert not os.path.isfile('output.dat_0')
+    with open('output.dat') as outfile:
+        next(outfile)
+        next(outfile)
+        line = outfile.readline()
+        assert line.split()[2] == '-3.22555'
+    os.remove('output.dat')
