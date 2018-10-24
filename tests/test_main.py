fe3a544f91e24a5f4f3cfff63e9bb6972f6af8b7
menta <mtomin.chem@gmail.com>
2018-10-24 11:27:20 +0200

    Fixed unit tests
    Signed-off-by: menta <mtomin.chem@gmail.com>

--- tests/test_main.py
+++ tests/test_main.py
@@ -159,12 +159,12 @@ def test_totalgui_plot():
 def test_totalgui_plot_generate_settings_pane():
     testroot = tk.Tk()
     testplot = TestPlot(testroot)
-    testgui=r.TotalGui
-    testgui.Plot.generate_settings_pane(testplot)
+    testgui = r.TotalGui(master=testroot)
+    r.TotalGui.Plot.generate_settings_pane(testplot)
     assert testplot.plottype.get() == ''
     testplot.data.filename.set('randomfilename')
     assert testplot.plottype.get() == ''
-    testgui.Plot.generate_settings_pane(testplot)
+    r.TotalGui.Plot.generate_settings_pane(testplot)
     assert testplot.plottype.get() == 'time'
     assert testplot.interactive.get() == 'False'
 
